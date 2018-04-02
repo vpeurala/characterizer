316d1033671997b3cea7cf29858be64db7ea7ee7
Ville Peurala <ville.peurala@gmail.com>
2018-04-02 17:04:07 +0300

    Can be run from CLI

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,17 @@ module Lib
     ( someFunc
     ) where
 
+import qualified Data.Either as E
+import qualified Language.Java.Parser as J
+import qualified System.Environment as SE
+import qualified System.IO as SI
+
 someFunc :: IO ()
-someFunc = putStrLn "Hello World"
+someFunc = do
+  args <- SE.getArgs
+  fileContent <- SI.readFile (args !! 0)
+  let parseResult = J.parser J.compilationUnit fileContent
+  case parseResult of
+    E.Left error -> putStrLn $ "Error: " ++ show error
+    E.Right compilationUnit -> putStrLn $ "CompilationUnit: " ++ show compilationUnit
+  putStrLn "Something parsed!"
