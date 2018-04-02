6d8930a44a1fc8bacfa8969737db3d1dd6af188b
Ville Peurala <ville.peurala@gmail.com>
2018-04-02 17:29:07 +0300

    Add test for MainExample

---
+++ test/MainExample.java
@@ -0,0 +1,5 @@
+public class MainExample {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hoo vee");
+    }
+}
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,3 +8,6 @@ main = hspec $ do
     it "Can parse a minimal Java file" $ do
       parseResult <- parseJavaFile "test/MinimalExample.java"
       (show parseResult) `shouldBe` "Right (CompilationUnit Nothing [] [ClassTypeDecl (ClassDecl [public] (Ident \"MinimalExample\") [] Nothing [] (ClassBody []))])"
+    it "Can parse a Java file with main" $ do
+      parseResult <- parseJavaFile "test/MainExample.java"
+      (show parseResult) `shouldBe` "Right (CompilationUnit Nothing [] [ClassTypeDecl (ClassDecl [public] (Ident \"MainExample\") [] Nothing [] (ClassBody [MemberDecl (MethodDecl [public,static] [] Nothing (Ident \"main\") [FormalParam [] (RefType (ArrayType (RefType (ClassRefType (ClassType [(Ident \"String\",[])]))))) False (VarId (Ident \"args\"))] [ClassRefType (ClassType [(Ident \"Exception\",[])])] Nothing (MethodBody (Just (Block [BlockStmt (ExpStmt (MethodInv (MethodCall (Name [Ident \"System\",Ident \"out\",Ident \"println\"]) [Lit (String \"Hoo vee\")])))]))))]))])"
