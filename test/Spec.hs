ed6d87326cd9e929e7832d29160070e7ce754fea
Ville Peurala <ville.peurala@gmail.com>
2018-04-02 17:23:49 +0300

    Better naming of test Java file

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,5 +6,5 @@ main :: IO ()
 main = hspec $ do
   describe "Characterizer.parseJavaFile" $ do
     it "Can parse a minimal Java file" $ do
-      parseResult <- parseJavaFile "test/Foo.java"
-      (show parseResult) `shouldBe` "Right (CompilationUnit Nothing [] [ClassTypeDecl (ClassDecl [public] (Ident \"Foo\") [] Nothing [] (ClassBody []))])"
+      parseResult <- parseJavaFile "test/MinimalExample.java"
+      (show parseResult) `shouldBe` "Right (CompilationUnit Nothing [] [ClassTypeDecl (ClassDecl [public] (Ident \"MinimalExample\") [] Nothing [] (ClassBody []))])"
