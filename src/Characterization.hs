c953b8d02fb606fa87146350fe5a65f0079a56a4
Ville Peurala <ville.peurala@gmail.com>
2018-04-02 22:41:55 +0300

    Recurse into subdirs

--- src/Characterization.hs
+++ src/Characterization.hs
@@ -1,12 +1,16 @@
 module Characterization
     ( main
     , parseJavaFile
+    , parseJavaFiles
     ) where
 
 import qualified Control.Exception as CE
+import qualified Control.Monad as CM
 import qualified Data.Either as DE
+import qualified Data.String.Utils as DSU
 import qualified Language.Java.Parser as J
 import qualified Language.Java.Syntax as J
+import qualified System.Directory as SD
 import qualified System.Environment as SE
 import qualified System.IO as SI
 import qualified Text.Parsec.Error as PE
@@ -14,11 +18,33 @@ import qualified Text.Parsec.Error as PE
 main :: IO ()
 main = do
   args <- SE.getArgs
-  parseResult <- parseJavaFile $ args !! 0
+  parseResult <- parseJavaFiles $ args !! 0
   putStrLn $ show parseResult
 
-parseJavaFile :: String -> IO (Either PE.ParseError J.CompilationUnit)
+parseJavaFile :: FilePath -> IO (Either PE.ParseError J.CompilationUnit)
 parseJavaFile fileName = do
   fileContent <- SI.readFile fileName
   let parseResult = J.parser J.compilationUnit fileContent
   return parseResult
+
+parseJavaFiles :: FilePath -> IO (Either [PE.ParseError] [J.CompilationUnit])
+parseJavaFiles dir = do
+  results <- recurseDirs dir
+  let lefts = DE.lefts results
+  if null lefts
+  then return $ DE.Right $ DE.rights results
+  else return $ DE.Left lefts
+  where recurseDirs :: FilePath -> IO [Either PE.ParseError J.CompilationUnit]
+        recurseDirs dir = do
+          entries <- SD.listDirectory dir
+          let filteredEntries = filter (`notElem` [".", ".."]) entries
+          mappedEntries <- CM.mapM handleEntry filteredEntries
+          return $ concat mappedEntries
+        handleEntry :: FilePath -> IO [Either PE.ParseError J.CompilationUnit]
+        handleEntry entry = do
+          isDir <- SD.doesDirectoryExist entry
+          if isDir
+          then recurseDirs entry
+          else if entry `DSU.endswith` ".java"
+          then sequence $ [parseJavaFile entry]
+          else return []
