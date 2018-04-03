070fa2af059b32e511885ca357573effed13f6c4
Ville Peurala <ville.peurala@gmail.com>
2018-04-03 07:34:04 +0300

    WIP. Failing test for parseJavaFiles.

--- test/CharacterizationSpec.hs
+++ test/CharacterizationSpec.hs
@@ -13,3 +13,8 @@ spec = do
     it "Can parse a Java file with main" $ do
       parseResult <- parseJavaFile "test/MainExample.java"
       (show parseResult) `shouldBe` "Right (CompilationUnit Nothing [] [ClassTypeDecl (ClassDecl [public] (Ident \"MainExample\") [] Nothing [] (ClassBody [MemberDecl (MethodDecl [public,static] [] Nothing (Ident \"main\") [FormalParam [] (RefType (ArrayType (RefType (ClassRefType (ClassType [(Ident \"String\",[])]))))) False (VarId (Ident \"args\"))] [ClassRefType (ClassType [(Ident \"Exception\",[])])] Nothing (MethodBody (Just (Block [BlockStmt (ExpStmt (MethodInv (MethodCall (Name [Ident \"System\",Ident \"out\",Ident \"println\"]) [Lit (String \"Hoo vee\")])))]))))]))])"
+  describe "Characterizer.parseJavaFiles" $ do
+    it "Can parse all examples" $ do
+      parseResult <- parseJavaFiles "test"
+      case parseResult of
+        Right results -> results `shouldNotBe` []
